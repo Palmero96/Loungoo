a8cb568a4db52803cee633c1f4227f56c826e11a
Palmero96 <38357897+Palmero96@users.noreply.github.com>
2018-06-07 01:42:11 +0200

    a

--- src/Mundo.cpp
+++ src/Mundo.cpp
@@ -12,6 +12,8 @@ void Mundo::Inicializa() {
 	protagonista->setVelocidad_modulo(75);
 
 	mapa = new Mapa("sources/Mapa1.txt", "images/Mapa1.png", "images/Mapa1_2.png");
+
+	glOrtho((vista.getx() - 320) / zoom, (vista.getx() + 320) / zoom, (vista.gety() - 240) / zoom, (vista.gety() + 240) / zoom, -1, 1);
 }
 
 void Mundo::Dibuja() {
